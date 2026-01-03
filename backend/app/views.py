03717b0c99500c5ba9898cbf27e04a4828fd3fdb
dzakyatha <18223124@std.stei.itb.ac.id>
2026-01-03 18:21:15 +0700

    feat: Update GoogleLogin callback_url to use environment variable for flexibility in views.py

--- backend/app/views.py
+++ backend/app/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from dj_rest_auth.registration.views import SocialLoginView
+from decouple import config
 
 def home(request):
     return HttpResponse("Hello, world!")
@@ -12,5 +13,5 @@ class GoogleLogin(SocialLoginView):
     Ensure callback_url matches the frontend and Google Console settings
     """
     adapter_class = GoogleOAuth2Adapter
-    callback_url = "http://localhost:5173" # for local development
+    callback_url = config('GOOGLE_OAUTH_CALLBACK_URL', default='http://localhost:5173')
     client_class = OAuth2Client
