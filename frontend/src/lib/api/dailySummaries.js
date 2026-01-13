413d6d8ccf93715af75921c907f89d61154a5524
Vaughn Daniel Kenneth <vaughndanielsiburian@gmail.com>
2026-01-13 16:56:47 +0900

    Configure dailySummaries.js to fix baseurl naming

--- frontend/src/lib/api/dailySummaries.js
+++ frontend/src/lib/api/dailySummaries.js
@@ -1,11 +1,17 @@
+// src/lib/api/dailySummaries.js
 import api from '../axios'
 
 export async function fetchDailySummaries(params = {}) {
-  const response = await api.get('/api/daily-summaries/', { params })
+  // Base URL axios sudah /api, jadi cukup /daily-summaries/
+  const response = await api.get('/daily-summaries/', { params })
   return response.data
 }
 
 export async function fetchPaymentBreakdown(params = {}) {
-  const response = await api.get('/api/daily-summaries/payment_breakdown/', { params })
+  // Endpoint untuk pie chart Sumber Pendapatan
+  const response = await api.get(
+    '/daily-summaries/payment_breakdown/',
+    { params },
+  )
   return response.data
 }
